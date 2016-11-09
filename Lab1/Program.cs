0c51b9e365791691889d7428b4eaa6bdb8f52065
Alexander Komissarov <alexander.komissarov@aol.co.uk>
2016-11-10 01:29:36 +0700

    Added metrics calculation
    Added confusion matrix, precision and recall calculations

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -62,41 +62,132 @@ namespace Lab1
             }
         }
 
+        public static int[,] CalcConfusionMatrix( MetricClassifier classifier, 
+                                                  Vector[] test, 
+                                                  string[] answers,
+                                                  string[] classes )
+        {
+            var matrix = new int[classes.Length, classes.Length];
+            var classIndexes = new Dictionary<string, int>( );
+            for ( int i = 0; i < classes.Length; ++i )
+            { classIndexes.Add( classes[i], i ); }
+
+            var classified = classifier.Classify( test );
+
+            for ( int i = 0; i < classified.Length; ++i )
+            { 
+                ++matrix[classIndexes[classified[i]], classIndexes[answers[i]]]; 
+            }
+
+            return matrix;
+        }
+
+        public static double[] CalcPrecision( int[,] confusionMatrix )
+        {
+            var result = new double[confusionMatrix.GetLength( 0 )];
+            for ( int i = 0; i < result.Length; ++i )
+            {
+                double sum = 0.0;
+                for ( int j = 0; j < confusionMatrix.GetLength( 1 ); ++j )
+                { sum += confusionMatrix[i, j]; }
+
+                result[i] = confusionMatrix[i, i] / sum; 
+            }
+
+            return result;
+        }
+
+        public static double[] CalcRecall( int[,] confusionMatrix )
+        {
+            var result = new double[confusionMatrix.GetLength( 1 )];
+            for ( int i = 0; i < result.Length; ++i )
+            {
+                double sum = 0.0;
+                for ( int j = 0; j < confusionMatrix.GetLength( 0 ); ++j )
+                { sum += confusionMatrix[j, i]; }
+
+                result[i] = confusionMatrix[i, i] / sum; 
+            }
+
+            return result;
+        }
+
+        public static void PrintMatrix<T>( T[,] matrix )
+        {
+            Console.WriteLine( "[" );
+            for ( int i = 0; i < matrix.GetLength( 0 ); ++i )
+            {
+                Console.Write( "\t[" );
+                if ( matrix.GetLength( 1 ) == 0 )
+                {
+                    Console.Write( "]" );
+                    break;
+                }
+
+                Console.Write( $" {matrix[i, 0]}" );
+
+                for ( int j = 1; j < matrix.GetLength( 1 ); ++j )
+                {
+                    Console.Write( $", {matrix[i, j]}" );
+                }
+                Console.WriteLine( " ]" );
+            }
+            Console.WriteLine( "]" );
+        }
+
         public static void Main( )
         {
             Vector[] data, test;
             string[] classes, testAns;
             GetData( out data, out classes, out test, out testAns );
             
-            //var classifier = new KNearestClassifier( 1, data, classes );
-            // var classifier = new KWeightedNearestClassifier( 10,
-            //                                                  KWeightedNearestClassifier.LinearWeight( 10 ), 
-            //                                                  data, 
-            //                                                  classes );
-            // var classifier = new ParzenWindowClassifier( ParzenWindowClassifier.GaussKernel,
-            //                                              0.35,
-            //                                              data,
-            //                                              classes );
+            var distinctClasses = classes.Concat( testAns ).Distinct( ).ToArray( );
 
-            // var classifier = new AdaptiveParzenClassifier( ParzenWindowClassifier.EpanechnikovKernel,
-            //                                                2,
-            //                                                data,
-            //                                                classes );
-
-            var classifier = new PotentialFunctionClassifier( 0.01,
-                                                              data,
-                                                              classes,
-                                                              data.Select( x => 0.35 ).ToArray( ) );
-
-            var ans = classifier.Classify( test );
-
-            int errorCount = 0;
-            for ( int i = 0; i < test.Length; ++i )
+            var classifiers = new List<MetricClassifier>
             {
-                if ( ans[i] != testAns[i] )
-                { ++errorCount; }
+                new KNearestClassifier( 1, data, classes ),
+                new KNearestClassifier( 5, data, classes ),
+                new KWeightedNearestClassifier( 10,
+                                                KWeightedNearestClassifier.LinearWeight( 10 ), 
+                                                data, 
+                                                classes ),
+                new ParzenWindowClassifier( ParzenWindowClassifier.GaussKernel,
+                                            0.35,
+                                            data,
+                                            classes ),
+                new AdaptiveParzenClassifier( ParzenWindowClassifier.EpanechnikovKernel,
+                                              2,
+                                              data,
+                                              classes ),
+                new PotentialFunctionClassifier( 0.01,
+                                                 data,
+                                                 classes,
+                                                 data.Select( x => 0.35 ).ToArray( ) )
+            };
+
+            var evalutions = from classifier in classifiers
+                             let confusionMatrix = CalcConfusionMatrix( classifier, test, testAns, distinctClasses )
+                             select new
+                             {
+                                 ConfusionMatrix = confusionMatrix,
+                                 Precisions = CalcPrecision( confusionMatrix ),
+                                 Recalls = CalcRecall( confusionMatrix ),
+                                 Classifier = classifier
+                             };
+
+            foreach ( var e in evalutions )
+            {
+                Console.WriteLine( $"--- {e.Classifier}:" );
+
+                PrintMatrix( e.ConfusionMatrix );
+
+                for ( int i = 0; i < distinctClasses.Length; ++i )
+                {
+                    Console.WriteLine( $"Class {distinctClasses[i]}:" );
+                    Console.WriteLine( $"\tPrecision: {e.Precisions[i]}" );
+                    Console.WriteLine( $"\tRecall: {e.Recalls[i]}" );
+                }
             }
-            Console.WriteLine( errorCount );
         }
     }
 }
